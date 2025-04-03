d0c114c874c61a0522f0887b35a4bbe81eab7125
severogianluca <gianlucasevero300@gmail.com>
2025-04-03 18:09:29 +0200

    Feat: create main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import './App.css'
 import Header from "./components/Header.jsx"
-
+import Main from "./components/Main.jsx"
 
 function App() {
 
@@ -13,7 +13,7 @@ function App() {
   </header>
 
   <main>
-
+    <Main/>
   </main>
 
   <footer>
---
+++ src/components/Main.jsx
@@ -0,0 +1,41 @@
+import digitalComics from '../assets/img/buy-comics-digital-comics.png';
+import dcMerchendise from '../assets/img/buy-comics-merchandise.png';
+import subscription from '../assets/img/buy-comics-subscriptions.png';
+import comicShopLocator from '../assets/img/buy-comics-shop-locator.png';
+import DcPowerVis from '../assets/img/buy-dc-power-visa.svg';
+function Main(){
+    return(
+        <>
+            <h2 className="bg-black">--&gt; Content goes here &lt;--</h2>
+            <div className= "bg-blue">
+            <ul>
+                <li>
+                    <img src= {digitalComics} alt="digital comics" />
+                    <p>DIGITAL COMICS</p>
+                </li>
+                <li>
+                    <img src= {dcMerchendise} alt=" merchendise" />
+                    <p>DC MERCHENDISE</p>
+                </li>
+                <li>
+                    <img src= {subscription} alt="subscription" />
+                    <p>SUBSCRIPTION</p>
+                </li>
+                <li>
+                    <img src={comicShopLocator} alt="shop locator" />
+                    <p>COMIC SHOP LOCATOR</p>
+                </li>
+                <li>
+                    <img src={DcPowerVis} alt="visa" />
+                    <p>DC POWER VISA</p>
+                </li>
+            </ul>
+            </div>
+
+        </>
+
+    )
+}
+
+
+export default Main
