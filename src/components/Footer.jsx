26a7063acab2782a5c7fd7dc2fdfe595d18e8002
severogianluca <gianlucasevero300@gmail.com>
2025-04-03 22:44:14 +0200

    Feat: create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,10 @@
 import bgLogo from "../assets/img/dc-logo-bg.png"
+import Facebook from "../assets/img/footer-facebook.png"
+import Twitter from "../assets/img/footer-twitter.png"
+import Youtube from "../assets/img/footer-youtube.png"
+import Pinterest from "../assets/img/footer-pinterest.png"
+import Periscope from "../assets/img/footer-pinterest.png"
+
 function Footer() {
     return (
         <>
@@ -55,7 +61,18 @@ function Footer() {
             </div>
 
             <div className="footer-bottom">
+                <div>
+                    <button className="btn">SIGN-UP NOW!</button>
+                </div>
 
+                <div className="flex-icon">
+                    <h3 className="footer-h3">FOLLOW US</h3>
+                    <img src={Facebook} alt="facebook" />
+                    <img src={Twitter} alt="twitter" />
+                    <img src={Youtube} alt="youtube" />
+                    <img src={Pinterest} alt="pinterest" />
+                    <img src={Periscope} alt="periscope" />
+                </div>
             </div>
         </>
 
