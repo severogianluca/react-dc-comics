77511b2d225db86e4592e2433d4a90aea2b64f21
severogianluca <gianlucasevero300@gmail.com>
2025-04-03 15:22:51 +0200

    Feat: creat function and insert the logo

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import Logo from "./Logo.jsx"
+
+function Header() {
+
+    return (
+        <>
+            <figure>
+                <Logo/>
+            </figure>  
+        </>
+    )
+
+
+}
+
+export default Header
---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/img/dc-logo.png"
+function Logo(){
+    return(
+        <>
+            <img src= {logo} alt="logo" />
+        </>
+    )
+}
+
+export default Logo
